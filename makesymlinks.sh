497c25ebbc8faa0a0854352636efc16a326e87e4
ciuncan <ceyhuncanu@gmail.com>
2013-10-13 05:43:31 +0300

    Fish shell added.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=$HOME/dotfiles                    # dotfiles directory
 olddir=$HOME/dotfiles_old             # old dotfiles backup directory
-files="bashrc gitconfig vimperatorrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
+files="bashrc gitconfig vimperatorrc vimrc vim zshrc oh-my-zsh oh-my-fish private scrotwm.conf Xresources config/fish"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -56,4 +56,31 @@ else
 fi
 }
 
+function install_fish {
+# Test to see if zshell is installed.  If it is:
+if [ -f /bin/fish -o -f /usr/bin/fish ]; then
+    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+    if [[ ! -d $dir/oh-my-zsh/ ]]; then
+        git clone https://github.com/bpinto/oh-my-fish.git
+    fi
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+        chsh -s $(which zsh)
+    fi
+else
+    # If zsh isn't installed, get the platform of the current machine
+    platform=$(uname);
+    # If the platform is Linux, try an apt-get to install zsh and then recurse
+    if [[ $platform == 'Linux' ]]; then
+        sudo apt-get install fish
+        install_fish
+    # If the platform is OS X, tell the user to install zsh :)
+    elif [[ $platform == 'Darwin' ]]; then
+        echo "Please install fish, then re-run this script!"
+        exit
+    fi
+fi
+}
+
 install_zsh
+install_fish
