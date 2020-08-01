fa3c19cf4570f931964c28609cb8511b682a0a6d
ciuncan <ceyhuncanu@gmail.com>
2020-08-01 16:12:44 +0300

    Add yaml and remote extensions to vs code installations

--- installation_list.sh
+++ installation_list.sh
@@ -38,19 +38,23 @@ rustup component add rust-src rls
 cargo install exa
 
 # Install vscode
-sudo snap install code --classic
 code --install-extension 13xforever.language-x86-64-assembly
 code --install-extension 2gua.rainbow-brackets
 code --install-extension bungcip.better-toml
 code --install-extension eamodio.gitlens
 code --install-extension formulahendry.code-runner
 code --install-extension humao.rest-client
-code --install-extension james-yu.latex-workshop
+code --install-extension James-Yu.latex-workshop
 code --install-extension mechatroner.rainbow-csv
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
 code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
 code --install-extension oderwat.indent-rainbow
-code --install-extension rreverser.llvm
+code --install-extension redhat.vscode-yaml
+code --install-extension RReverser.llvm
 code --install-extension rust-lang.rust
+code --install-extension scala-lang.scala
 code --install-extension scalameta.metals
 code --install-extension serayuzgur.crates
 code --install-extension streetsidesoftware.code-spell-checker
